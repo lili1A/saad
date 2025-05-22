8720568b14670bd3ec1232e9682ad6b378634b70
lili1A <154353393+lili1A@users.noreply.github.com>
2025-05-22 14:00:00 +0800

    Add files via upload

---
+++ saad/main.js
@@ -0,0 +1,57 @@
+// function: show password in the password placeholder 
+function togglePasswordVisibility() {
+    var passwordField = document.getElementById("password");
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+    } else {
+        passwordField.type = "password";
+    }
+}
+
+// variable: authorized users 
+var authorizedUsers = [
+    {username:"Sysadmin",password:"123"},
+    {username:"Doctor",password:"456"},
+    {username:"Client",password:"789"},
+]
+
+// varaible: show password checkbox 
+var showPasswordCheckbox = document.getElementById("showPassword");
+
+// when checkbox is clicked, show password visibility
+showPasswordCheckbox.addEventListener("change", function() {
+    togglePasswordVisibility();
+});
+
+// fucntion: capitalize fisrt letter in the username
+function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function fixLettersCase() {
+    var usernameInput = document.getElementById("username");
+    var usernameValue = usernameInput.value.trim();
+    
+    usernameInput.value = usernameValue.charAt(0).toUpperCase() + usernameValue.slice(1).toLowerCase();
+}
+
+// fucntion: redirect user based on his password
+function redirectUser() {
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value;
+
+    // redirection based on the password 
+    switch (password) {
+        case "123":
+            window.location.href = "admin.html";
+            break;
+        case "456":
+            window.location.href = "doctor.html";
+            break;
+        case "789":
+            window.location.href = "patient.html";
+            break;
+        default:
+            alert("Invalid password. Access denied.");
+    }
+}
